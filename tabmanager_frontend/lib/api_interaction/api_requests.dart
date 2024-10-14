24d8725dfc8b7e7dd4d391393663940ea8f09c1b
Sami <72280649+Sami-ul@users.noreply.github.com>
2024-10-14 02:00:02 -0600

    Add comments to api_requests.dart

--- tabmanager_frontend/lib/api_interaction/api_requests.dart
+++ tabmanager_frontend/lib/api_interaction/api_requests.dart
@@ -6,16 +6,19 @@ class APIRequests {
   APIRequests();
 
   static void addLink(Link link) async {
+    // take in a link, convert to json and add it to the database
     String url = "http://localhost:3001/links";
     await http.post(Uri.parse(url), body: link.toJson());
   }
 
   static Future<void> removeLink(Link link) async {
+    // create a delete request to the backend using the link id to identify
     String url = "http://localhost:3001/links/${link.id}";
     await http.delete(Uri.parse(url), body: link.toJson());
   }
 
   static Stream<List<Link>> getLinks() async* {
+    // load lists as a stream
     String url = "http://localhost:3001/links";
     while (true) {
       await Future.delayed(const Duration(seconds: 1)); // 1 second delay
